6f5834278394d3d481e74c5ee2b3686e36a43b78
Федоров Данила <FedorovD@ntcmik.local>
2023-10-30 11:23:49 +0300

    ver4.1

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,6 +1,22 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/DanilaFedGit/restaurant/database"
+	"github.com/DanilaFedGit/restaurant/models"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var validate = validator.New()
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -9,7 +25,15 @@ func GetFoods() gin.HandlerFunc {
 }
 func GetFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		foodid := c.Param("food_id")
+		var food models.Food
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodid}).Decode(&food)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error with find food"})
+		}
+		c.JSON(http.StatusOK, food)
 	}
 }
 func UpdateFood() gin.HandlerFunc {
@@ -19,6 +43,27 @@ func UpdateFood() gin.HandlerFunc {
 }
 func CreateFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		var food models.Food
+		var menu models.Menu
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		err := validate.Struct(food)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		err = menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "menu wasn't found"})
+			return
+		}
+		food.Created_at = time.Parse(time.RFC3339, time.Now()).Format(time.RFC3339)
+		food.Update_at = time.Parse(time.RFC3339, time.Now()).Format(time.RFC3339)
+		food.ID = primitive.ObjectID()
 
 	}
 }
